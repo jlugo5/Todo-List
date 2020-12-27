c6558682acebf7a1cbc598744665360070ae2250
Johanthan <johanthan.lugo5@gmail.com>
2020-12-27 10:39:30 -0500

    Todo List

---
+++ todo_list/src/App.js
@@ -0,0 +1,26 @@
+// Build a Simple Todo App with React and Redux
+// Home Page - Input Box, Custom Button with an alert stating that todo added successfully on submit
+// Todo List Page - All the the todo list should be displayed
+// Build Reusable Components for Input and Button
+// Dependencies
+// React Router DOM
+// Redux
+// Redux Logger
+
+import './App.css'
+import React from 'react'
+
+class App extends React.Component{
+  render(){
+    return(
+      <div className='App'>
+        <h1>Todo List</h1>
+        <h2>Home Page</h2>
+        <h2>Todo List Page</h2>
+      </div>
+      
+    )
+  }
+}
+
+export default App
